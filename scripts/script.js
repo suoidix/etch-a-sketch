039d83a0cccb52c83308b6fbd91465310c093379
suoidix <130245469+suoidix@users.noreply.github.com>
2023-11-22 14:07:52 -0500

    Call function before creation

--- scripts/script.js
+++ scripts/script.js
@@ -8,22 +8,24 @@ let gridInt = parseInt(input);
 let gridSize = (gridInt*gridInt) + gridInt;
 let gridSizePlus = parseInt(gridInt) + 1;
 
+createGrid();
+
+
 //create 16x16 grid
 function createGrid(){
     for (i = 0; i < (gridSize); i++) {
         const div = document.createElement('div');
         div.id = 'box';
         container.appendChild(div);
-        const boxStyle = `
+    }
+    const boxStyle = `
         .container>div:nth-child(${gridSizePlus}n + 1) {
             width: 100%;
             border: 0;
             height: 0;
           }`
-        const styleSheet = document.createElement("style");
-        styleSheet.innerText = boxStyle
-        document.head.appendChild(styleSheet)
-    }
+    const styleSheet = document.createElement("style");
+    styleSheet.innerText = boxStyle
+    document.head.appendChild(styleSheet)
 }
 
-createGrid();
