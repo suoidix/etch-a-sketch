8a5c7d7cdc275d69c070a7cc314eb275582d0dd2
suoidix <130245469+suoidix@users.noreply.github.com>
2023-11-22 12:56:40 -0500

    Update function for gridspace to accept dynamic input
    Added user prompt to accept custom grid size

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,26 @@
 //container for grid
 const container = document.querySelector(".container");
 
+let input = prompt ("Chose grid space!");
+let gridInt = parseInt(input);
+let gridSize = (gridInt*gridInt) + gridInt;
+let gridSizePlus = parseInt(gridInt) + 1;
+
 //create 16x16 grid
 function createGrid(){
-    for (i = 0; i <= 256; i++) {
+    for (i = 0; i < (gridSize); i++) {
         const div = document.createElement('div');
+        div.id = 'box';
         container.appendChild(div);
+        const boxStyle = `
+        .container>div:nth-child(${gridSizePlus}n + 1) {
+            width: 100%;
+            border: 0;
+            height: 0;
+          }`
+        const styleSheet = document.createElement("style");
+        styleSheet.innerText = boxStyle
+        document.head.appendChild(styleSheet)
     }
 }
 
