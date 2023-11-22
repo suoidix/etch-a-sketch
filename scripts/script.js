72c57a476135ba6b3ca1427dbffaa71a5e5f74bd
suoidix <130245469+suoidix@users.noreply.github.com>
2023-11-22 12:05:10 -0500

    Create function for grid

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
 //script for etch-a-sketch project
+
+//container for grid
+const container = document.querySelector(".container");
+
+//create 16x16 grid
+function createGrid(){
+    for (i = 0; i <= 256; i++) {
+        const div = document.createElement('div');
+        container.appendChild(div);
+    }
+}
+
+createGrid();
